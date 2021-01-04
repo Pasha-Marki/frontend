d49d75101fda6d592abb09be63c82d8adc35e35b
Pasha <PashaRMarki@gmail.com>
2021-01-04 03:42:12 +0000

    shiphawk link opens in new tab

--- app/home.js
+++ app/home.js
@@ -13,7 +13,7 @@ const HomePage = () => (
 
   <Fragment>
     <h1>Pasha Marki</h1>
-    <p>Welcome to my personal website. I graduated from the <a style={hiddenLinkStyle}>University of Redlands</a> with a B.S. in Physics and a minor in Math. I spent two years at <a href="https://www.shiphawk.com" style={hiddenLinkStyle}>Shiphawk</a> as a fullstack developer working on a React frontend and a Ruby on Rails with Postgres backend.</p>
+    <p>Welcome to my personal website. I graduated from the <a style={hiddenLinkStyle}>University of Redlands</a> with a B.S. in Physics and a minor in Math. I spent two years at <a href="https://www.shiphawk.com" target="_blank" rel="noopener noreferrer" style={hiddenLinkStyle}>Shiphawk</a> as a fullstack developer working on a React frontend and a Ruby on Rails with Postgres backend.</p>
   </Fragment>
 
 );
